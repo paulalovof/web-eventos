c31943571e8ce5b57af6a15d1f2d58b104940e5f
paulalovof <paulafernanda.lovo@gmail.com>
2023-11-24 11:35:33 -0300

    js com cards, e abrindo outra tela, validando formulario

---
+++ js/adotar.js
@@ -0,0 +1,55 @@
+let formulario = document.querySelector('form');
+
+const validarDados = (nome, telefone, email, renda, opcao, intencao) => {
+  let control = true;
+
+  if(nome.value.trim() == ""){
+    nome.style.backgroundColor = "red";
+    control = false;
+  }
+
+  if(telefone.value.trim() == ""){
+    telefone.style.backgroundColor = "red";
+    control = false;
+  }
+
+  if(email.value.trim() == ""){
+    email.style.backgroundColor = "red";
+    control = false;
+  }
+
+  if(renda == null){
+    let erro = document.getElementById('#erro-renda');
+    erro.classList.add('msgm-erro--active');
+  }
+
+  return control;
+}
+
+formulario.addEventListener("submit", (event) => {
+
+  let nome = document.querySelector('#nome');
+  let telefone = document.querySelector('#telefone');
+  let email = document.querySelector('#email');
+  let renda = document.querySelector('input[name="renda"]:checked');
+  let residencia = document.querySelector('#residencia');
+  let opcao = residencia.options[residencia.selectedIndex];
+  let intencao = document.querySelector('#intencao');
+
+  if(validarDados(nome, telefone, email, renda, opcao, intencao)){
+    let adotante = {
+      nome: nome.value.trim(),
+      telefone: telefone.value.trim(),
+      email: email.value.trim(),
+      renda: renda.value,
+      residencia: opcao.value,
+      intencao: intencao.value.trim(),
+      idAnimal: "1",
+    };
+
+    console.log(adotante);
+  }else{
+    event.preventDefault();
+  }
+
+});
---
+++ js/script.js
@@ -0,0 +1,77 @@
+let listaAnimais = [
+    {
+        idAnimal: 1,
+        nome: "Barão",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diamnisl dapibus quis dictum quis, facilisis non risus. Aliquam temponisi id lacinia lobortis. Donec in justo mauris. Mauris luctus tristique est, a faucibus elit. Vivamus et dapibus mi, id facilisis",
+        img: "./img/img1.webp"
+    },
+    {
+        idAnimal: 2,
+        nome: "Nami",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diamnisl dapibus quis dictum quis, facilisis non risus. Aliquam temponisi id lacinia lobortis. Donec in justo mauris. Mauris luctus tristique est, a faucibus elit. Vivamus et dapibus mi, id facilisis",
+        img: "./img/img2.jpg"
+    },
+    {
+        idAnimal: 3,
+        nome: "Leozinho",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diamnisl dapibus quis dictum quis, facilisis non risus. Aliquam temponisi id lacinia lobortis. Donec in justo mauris. Mauris luctus tristique est, a faucibus elit. Vivamus et dapibus mi, id facilisis",
+        img: "./img/img3.webp"
+    },
+    {
+        idAnimal: 4,
+        nome: "Tiquinha",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diamnisl dapibus quis dictum quis, facilisis non risus. Aliquam temponisi id lacinia lobortis. Donec in justo mauris. Mauris luctus tristique est, a faucibus elit. Vivamus et dapibus mi, id facilisis",
+        img: "./img/img4.jpg"
+    },
+    {
+        idAnimal: 5,
+        nome: "Senna",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diamnisl dapibus quis dictum quis, facilisis non risus. Aliquam temponisi id lacinia lobortis. Donec in justo mauris. Mauris luctus tristique est, a faucibus elit. Vivamus et dapibus mi, id facilisis",
+        img: "./img/img5.webp"
+    }
+];
+
+let container = document.querySelector('.container');
+
+const criarCards = () => {
+    listaAnimais.forEach((elemento) => {
+        let card = 
+        `
+        <div class="card">
+            <img class="card-img" src="${elemento.img}" />
+            <h2 class="card-titulo">${elemento.nome}</h2>
+            <p class="card-descricao">${elemento.descricao}
+            </p>
+            <a class="card-botao" idAnimal="${elemento.idAnimal}"> ADOTAR </a>
+        </div>
+        `;
+        container.innerHTML += card;
+    });
+};
+
+window.addEventListener("load", () => {
+    criarCards();
+
+    let cards = document.querySelectorAll('.card');
+    cards.forEach((elemento) => {
+        elemento.addEventListener("mouseover", () => {
+            elemento.classList.add('change-scale');
+        });
+
+        elemento.addEventListener("mouseout", () => {
+            elemento.classList.remove('change-scale');
+        });
+
+        elemento.lastElementChild.addEventListener("click", (event) => {
+            event.preventDefault();
+            elemento.lastElementChild.getAttribute('idAnimal');
+
+            alert(elemento.lastElementChild.getAttribute('idAnimal'));
+            
+            window.location.href = "./adotar.html";
+            
+        });
+    });
+});
+
+//container.classList.add('change-background');
